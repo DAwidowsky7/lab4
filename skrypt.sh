7efd68f3c7b0f462769df3dc20519ebfc20c6e5f
„DAwidowsky7” <fanciak@wp.pl>
2022-04-23 00:19:02 +0200

    Myslnik piaty

--- skrypt.sh
+++ skrypt.sh
@@ -1,22 +1,11 @@
-echo $dzisiaj
-fi
+#!/bin/bash
 
-if [ "$1" == "--logs" ];
-if [ "$1" == "--logs" ] && [ -n "$2" ];
-then
-for i in $(seq $2)
-do
-	echo log$i >> log$i.txt
-	echo $0 >> log$i.txt
-	echo $dzisiaj >> log$i.txt
-done
-elif [ "$1" == "--logs" ];
-then
-b=100
-for i in $(seq 100)
-do
-	echo log$i >> log$i.txt
-	echo $0 >> log$i.txt
-	echo $dzisiaj >> log$i.txt
-done
+if [ "$1" == "--help" ]; then
+echo "Dostepne opcje:  "
+echo "--date wyswietla aktualna date"
+echo "--logs LICZBA tworzy LICZBA plikow o nazwie logLICZBA.txt i wpisuje do nich dane"
+echo "--logs jak wyzej ale tworzy 100 plikow"
 fi
+dzisiaj=$(date)
+if [ "$1" == "--date" ]; 
+then
