201be832c4a44bedf992418d988739469c440252
„DAwidowsky7” <fanciak@wp.pl>
2022-04-23 00:17:57 +0200

    Myslnik trzeci

--- skrypt.sh
+++ skrypt.sh
@@ -1,1 +1,14 @@
-*log*.*
+then
+echo $dzisiaj
+fi
+
+if [ "$1" == "--logs" ];
+then
+b=100
+for i in $(seq 100)
+do
+	echo log$i >> log$i.txt
+	echo $0 >> log$i.txt
+	echo $dzisiaj >> log$i.txt
+done
+fi
