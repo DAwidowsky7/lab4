0734bf55bfe16d0dca502ab38069202164fd92a0
„DAwidowsky7” <fanciak@wp.pl>
2022-04-23 00:24:47 +0200

    Zmiana gitignore

--- skrypt.sh
+++ skrypt.sh
@@ -1,26 +1,3 @@
-done
-fi
-
-if [ "$1" == "--init" ] || [ "$1" == "-i" ]]; then
-if [ "$1" == "--init" ] || [ "$1" == "-i" ]; then
-    git clone https://github.com/Kamil180411/lab4.git
-    export PATH="$PWD:$PATH"
-fi
-
-if [ "$1" == "--error" ] || [ "$1" == "-e" ] && [ -n "$2" ]; then
-    for i in $(seq $2); 
-	do
-		mkdir -p error$i 
-        echo "error$i.txt" >> error$i/error$i.txt
-		echo $0 >> error$i/error$i.txt
-		echo $dzisiaj >> error$i/error$i.txt
-    done
-elif [ "$1" == "--errors" ] || [ "$1" == "-e" ]; then
-    for i in $(seq 100); 
-	do
-		mkdir -p error$i 
-        echo "error$i.txt" >> error$i/error$i.txt
-		echo $0 >> error$i/error$i.txt
-		echo $dzisiaj >> error$i/error$i.txt
-    done
-fi 
+*log*.* 
+*log*.*
+*error*
