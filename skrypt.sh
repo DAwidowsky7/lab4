544764dba02811333e103fb01ddb4424da2bf81c
„DAwidowsky7” <fanciak@wp.pl>
2022-04-23 00:23:52 +0200

    --error dla 100 i 30 plikow

--- skrypt.sh
+++ skrypt.sh
@@ -1,13 +1,26 @@
-then
-	for i in $(seq $2)
+done
+fi
+
+if [ "$1" == "--init" ] || [ "$1" == "-i" ]]; then
+if [ "$1" == "--init" ] || [ "$1" == "-i" ]; then
+    git clone https://github.com/Kamil180411/lab4.git
+    export PATH="$PWD:$PATH"
+fi
+
+if [ "$1" == "--error" ] || [ "$1" == "-e" ] && [ -n "$2" ]; then
+    for i in $(seq $2); 
 	do
-		echo log$i >> log$i.txt
-		echo $0 >> log$i.txt
-		echo $dzisiaj >> log$i.txt
-		mkdir -p log$i 
-		echo log$i >> log$i/log$i.txt
-		echo $0 >> log$i/log$i.txt
-		echo $dzisiaj >> log$i/log$i.txt
-	done
-elif [ "$1" == "--logs" ] || [ "$1" == "-l" ];
-then
+		mkdir -p error$i 
+        echo "error$i.txt" >> error$i/error$i.txt
+		echo $0 >> error$i/error$i.txt
+		echo $dzisiaj >> error$i/error$i.txt
+    done
+elif [ "$1" == "--errors" ] || [ "$1" == "-e" ]; then
+    for i in $(seq 100); 
+	do
+		mkdir -p error$i 
+        echo "error$i.txt" >> error$i/error$i.txt
+		echo $0 >> error$i/error$i.txt
+		echo $dzisiaj >> error$i/error$i.txt
+    done
+fi 
