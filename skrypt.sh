a1717d6653b662868dfad612ee941f7712a4eb32
„DAwidowsky7” <fanciak@wp.pl>
2022-04-23 00:18:28 +0200

    Myslnik czwarty

--- skrypt.sh
+++ skrypt.sh
@@ -1,8 +1,16 @@
-then
 echo $dzisiaj
 fi
 
 if [ "$1" == "--logs" ];
+if [ "$1" == "--logs" ] && [ -n "$2" ];
+then
+for i in $(seq $2)
+do
+	echo log$i >> log$i.txt
+	echo $0 >> log$i.txt
+	echo $dzisiaj >> log$i.txt
+done
+elif [ "$1" == "--logs" ];
 then
 b=100
 for i in $(seq 100)
