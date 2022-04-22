af3c9b355c48ee5082459d9cc416835210e2f12f
„DAwidowsky7” <fanciak@wp.pl>
2022-04-23 00:23:06 +0200

    --logs dla 30 plikow

--- skrypt.sh
+++ skrypt.sh
@@ -1,12 +1,13 @@
 then
-	for i in $(seq 100)
-do
-	echo log$i >> log$i.txt
-	echo $0 >> log$i.txt
-	echo $dzisiaj >> log$i.txt
-	mkdir -p log$i 
-	echo "log$i.txt" >> log$i/log$i.txt
-	echo $0 >> log$i/log$i.txt
-	echo $dzisiaj >> log$i/log$i.txt
-done
-fi
+	for i in $(seq $2)
+	do
+		echo log$i >> log$i.txt
+		echo $0 >> log$i.txt
+		echo $dzisiaj >> log$i.txt
+		mkdir -p log$i 
+		echo log$i >> log$i/log$i.txt
+		echo $0 >> log$i/log$i.txt
+		echo $dzisiaj >> log$i/log$i.txt
+	done
+elif [ "$1" == "--logs" ] || [ "$1" == "-l" ];
+then
